f3870473fb4e44cfdfc25e4c3eee08ac240718e5
Christy-Lin <cl5501@nyu.edu>
2022-12-05 18:54:36 -0500

    Working Prototype

--- Final_Project/sketch.js
+++ Final_Project/sketch.js
@@ -1,16 +1,21 @@
 let player;
 let building;
+let birds;
+
+let buildings = [];
+
+// let command;
+
+let button;
 
 function setup() {
   createCanvas(800, 800);
   background(180, 238, 250);
 
-  // video = createCapture(VIDEO);
-  // video.size(800, 600);
-  // video.hide();
-
   rectMode(CORNER);
   circle(CENTER);
+
+  // command = new p5.SpeechRec('en-US');
 
   // Background Buildings
   // noStroke();
@@ -28,27 +33,71 @@ function setup() {
   // vertex(23, 480);
   // endShape(CLOSE);
 
+  // position = createVector(400, 400);
+  // velocity = createVector(random(-1, -5), 0);
+  // building1 = new Building(velocity);
+
+  // player_position = createVector()
   player = new Player();
-  building = new Building();
+  buildings.push(new Building())
+  birds = new Birds();
+  // building = new Building();
 }
 
 function draw() {
   background(180, 238, 250);
 
+  // The key is down functions will be replaced with speech recognition
+  // The input will be commands 'up' and 'down' and will call the player up() and down() methods respectively
   if (keyIsDown(UP_ARROW)) {
     player.up();
   }
-
+  
   if (keyIsDown(DOWN_ARROW)) {
     player.down();
   }
+  
+  if (random(10) < 0.03) {
+    buildings.push(new Building());
+  }
 
-  building.display();
-  building.update();
+  for (building of buildings) {
+    building.display(39, 193, 217);
+    building.update();
+  }
 
   player.display();
+
+  birds.display();
+  
+  if (player.hits(building)) {
+    noLoop();
+    background(0);
+    fill(255, 0, 0);
+    noStroke();
+    textSize(50);
+    textAlign(CENTER);
+    text('Game Over!', width/2, height/2);
+
+    button = createButton("Play again");
+    button.mousePressed(play_again);
+  }
 }
 
+function play_again() {
+  createCanvas(800, 800);
+  background(180, 238, 250);
+
+  for (building in buildings) {
+    buildings.pop();
+  }
+
+  rectMode(CORNER);
+  circle(CENTER);
+  player = new Player();
+  buildings.push(new Building())
+  loop();
+}
 
 class Building {
   constructor() {
@@ -73,6 +122,22 @@ class Building {
 
 }
 
+class Birds {
+  constructor() {
+
+  }
+  display(red, green, blue) {
+    fill(34, 181, 159);
+    stroke(102, 230, 250);
+
+    square(674, 26, 25);
+    square(637, 63, 25);
+    square(600, 100, 25);
+    square(637, 137, 25);
+  }
+
+}
+
 class Player {
   constructor() {
     this.x = 100;
@@ -85,6 +150,15 @@ class Player {
     stroke(70, 135, 201);
     fill(95, 191, 242);
     circle(this.x, this.y, this.size);
+  }
+
+  hits(building) {
+    // Detects if the player games contact with the buildings
+    let building_one = collideRectCircle(building.x_one, 325, 150, 475, this.x, this.y, this.size);
+    let building_two = collideRectCircle(building.x_two, 375, 150, 425, this.x, this.y, this.size);
+    let building_three = collideRectCircle(building.x_three, 450, 150, 400, this.x, this.y, this.size);
+
+    return building_one || building_two || building_three;
   }
 
   up() {
